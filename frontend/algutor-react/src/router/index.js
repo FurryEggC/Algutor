299dcf1037a216e74c4db75e8b553ad10ee1d09b
user <37504497@qq.com>
2025-12-21 19:16:03 +0800

    Update Execute

--- frontend/algutor-react/src/router/index.js
+++ frontend/algutor-react/src/router/index.js
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App.js'
 import ErrorPage from '../pages/error/index.js'
-import Answer from '../pages/answer/index.js'
-import Question from '../pages/question/index.js'
 import Knowledge from '../pages/knowledge/index.js'
+import Execute from '../pages/execute/index.js'
+import AI from '../pages/ai/index.js'
+import Explain from '../pages/ai/children/explain/index.js'
+import Generate from '../pages/ai/children/generate/index.js'
+import Debug from '../pages/ai/children/debug/index.js'
+import Solve from '../pages/ai/children/solve/index.js'
+
+
 
 const router = createBrowserRouter([
   {
@@ -15,12 +21,30 @@ const router = createBrowserRouter([
     element: <ErrorPage></ErrorPage>
   },
   {
-    path: '/answer',
-    element: <Answer></Answer>
+    path: '/ai',
+    element: <AI></AI>,
+    children: [
+      {
+        path: '/ai/explain',
+        element: <Explain></Explain>
+      },
+      {
+        path: '/ai/generate',
+        element: <Generate></Generate>
+      },
+      {
+        path: '/ai/debug',
+        element: <Debug></Debug>
+      },
+      {
+        path: '/ai/solve',
+        element: <Solve></Solve>
+      }
+    ]
   },
   {
-    path: '/question',
-    element: <Question></Question>
+    path: '/execute',
+    element: <Execute></Execute>
   },
   {
     path: '/knowledge',
